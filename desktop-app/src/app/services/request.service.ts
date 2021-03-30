63eb04c7f8313ae0377032156c0c100bf828fe5f
Rick Verkuijlen <i410126@fhict.nl>
2021-03-30 16:39:04 +0200

    Made the backend more open

--- desktop-app/src/app/services/request.service.ts
+++ desktop-app/src/app/services/request.service.ts
@@ -9,6 +9,6 @@ export class RequestService {
   constructor(private http: HttpClient) { }
 
   getSong() {
-    return this.http.get("http://localhost/hello-resteasy/download", {responseType: 'blob'});
+    return this.http.get("http://localhost/music/gpf|from-within|HEADHUNTERZ-FROM_WITHIN_(GPF_OFFICIAL_REMIX).mp3", {responseType: 'blob'});
   }
 }
--- music-service/src/main/java/nl/rickverkuijlen/hardhub/MusicController.java
+++ music-service/src/main/java/nl/rickverkuijlen/hardhub/MusicController.java
@@ -1,6 +1,7 @@
 package nl.rickverkuijlen.hardhub;
 
 import nl.rickverkuijlen.hardhub.S3.FileObject;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
@@ -19,23 +20,22 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Path("/hello-resteasy")
+@Path("/music")
 public class MusicController extends CommonResource {
     @Inject
     S3Client s3;
 
-    private String test = "music/HEADHUNTERZ - FROM WITHIN (GPF OFFICIAL REMIX).mp3";
-
     @GET
-    @Path("download")
+    @Path("{path}")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public Response downloadFile() {
+    public Response downloadFile(@PathParam String path) {
+        path = path.replace("|", "/");
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        GetObjectResponse object = s3.getObject(buildGetRequest(test), ResponseTransformer.toOutputStream(baos));
+        GetObjectResponse object = s3.getObject(buildGetRequest(path), ResponseTransformer.toOutputStream(baos));
 
         Response.ResponseBuilder response = Response.ok((StreamingOutput) baos::writeTo);
-        response.header("Content-Disposition", "attachment;'filename=" + test);
+        response.header("Content-Disposition", "attachment;'filename=" + path);
         response.header("Content-Type", object.contentType());
         return response.build();
     }
